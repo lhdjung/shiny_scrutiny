155d6f24caa489037cdf554c5c3f61a266ae7131
Lukas Jung <jung-lukas@gmx.net>
2024-05-09 21:04:50 +0200

    write packages.R script more clearly

--- scripts/packages.R
+++ scripts/packages.R
@@ -19,28 +19,32 @@ packages_missing <- packages_needed[!packages_needed %in% packages_installed]
 n_missing <- length(packages_missing)
 
 # If any packages are missing, inform the user that they need to be installed,
-# provide the code to do so, and throw an error:
+# provide the code to do so, and throw an error. The wording of the error
+# message is different if exactly one package is missing, which is conveyed by
+# the `missing1` flag.
 if (n_missing > 0) {
-  exactly_one_missing <- n_missing == 1
-  msg_package_s <- if (exactly_one_missing) "package" else "packages"
-  msg_it_them <- if (exactly_one_missing) "it" else "them"
-  msg_its_their <- if (exactly_one_missing) "its" else "their"
-  msg_open <- if (exactly_one_missing) "" else "c("
-  msg_close <- if (exactly_one_missing) "" else ")"
-  packages_missing <- paste0("\"", packages_missing, "\"", collapse = ", ")
-  message(paste(n_missing, "required", msg_package_s, "missing."))
+  missing1 <- n_missing == 1
+  msg_package_s <- if (missing1) "package" else "packages"
+  msg_it_them <- if (missing1) "it" else "them"
+  msg_its_their <- if (missing1) "its" else "their"
+  msg_c_open <- if (missing1) "" else "c("
+  msg_c_close <- if (missing1) "" else ")"
+  msg_package_names <- paste0("\"", packages_missing, "\"", collapse = ", ")
+  message(paste("Note:", n_missing, "required", msg_package_s, "missing."))
   message(paste(
     "Run this code to install", msg_it_them, "along with",
     msg_its_their, "dependencies:"
   ))
   cat(paste0(
-    "  install.packages(", msg_open, packages_missing, msg_close,
+    "  install.packages(", msg_c_open, msg_package_names, msg_c_close,
     ", dependencies = TRUE)\n"
   ))
   stop("Can't run app without required packages installed. See fix above.")
 }
 
-# Without an error, all packages are installed, so just attach them:
+# If the app gets here, all packages are already installed, so just attach them.
+# The option `character.only = TRUE` is needed because the package name is not
+# entered as a literal expression, as in `library(dplyr)`, but via a variable.
 for (i in seq_along(packages_needed)) {
   library(packages_needed[i], character.only = TRUE)
 }
